74676f39d6f852e059fb68f9a5a4e78b0ba102cc
Akilan M <akilan042005@gmail.com>
2025-08-22 17:26:22 +0000

    error

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,9 +1,10 @@
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(true)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
     const checkDevice = () => {
